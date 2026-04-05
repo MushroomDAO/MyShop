d1be9848b8df63efeb34fd6f53bc3351fe495b39
jhfnetboy <jhfnetboy@gmail.com>
2026-04-05 20:32:21 +0800

    fix(contracts): lock X402AccessAction pragma to 0.8.33

--- contracts/src/actions/X402AccessAction.sol
+++ contracts/src/actions/X402AccessAction.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity 0.8.33;
 
 interface IERC721Mintable {
     function mint(address to, string calldata uri) external returns (uint256);
